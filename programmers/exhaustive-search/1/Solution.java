71a8285e9ede4f0dec34c96248ed3f61e93de25e
luhavis <luhavisdev@gmail.com>
2021-06-17 01:46:29 +0900

    Feat : 완전탐색 (모의고사) 알고리즘 (#4)

--- programmers/exhaustive-search/1/Solution.java
+++ programmers/exhaustive-search/1/Solution.java
@@ -95,3 +95,62 @@ class Solution {
         return answer;
     }
 }
+
+class Solution2 {
+    public int[] solution(int[] answers) {
+        
+        int[] person1Pattern = {1, 2, 3, 4, 5};
+        int[] person2Pattern = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3Pattern = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+                
+        int person1Count = 0;
+        int person2Count = 0;
+        int person3Count = 0;
+        
+        int person1PatternIndex = 0;
+        int person2PatternIndex = 0;
+        int person3PatternIndex = 0;
+         
+        int answersLength = answers.length;
+        
+        for (int i=0; i<answersLength; i++) {                       
+            
+            if (answers[i] == person1Pattern[person1PatternIndex]) person1Count +=1;
+            if (answers[i] == person2Pattern[person2PatternIndex]) person2Count +=1;
+            if (answers[i] == person3Pattern[person3PatternIndex]) person3Count +=1;
+            
+            if (person1PatternIndex+1 > person1Pattern.length-1) person1PatternIndex = 0;
+            else person1PatternIndex +=1;
+            
+            if (person2PatternIndex+1 > person2Pattern.length-1) person2PatternIndex = 0;
+            else person2PatternIndex +=1;
+            
+            if (person3PatternIndex+1 > person3Pattern.length-1) person3PatternIndex = 0;
+            else person3PatternIndex +=1;            
+        }
+        
+        
+        int[] answer = null;
+        
+        if (person1Count == person2Count && person1Count == person3Count ) {
+            answer = new int[]{1, 2, 3};
+        } else if (person1Count > person2Count && person1Count > person3Count) {
+            answer = new int[]{1};
+        } else if (person2Count > person3Count && person2Count > person1Count) {
+            answer = new int[]{2};
+        } else if (person3Count > person1Count && person3Count > person2Count) {
+            answer = new int[]{3};
+        } else if (person1Count == person2Count && person1Count > person3Count) {
+            answer = new int[]{1, 2};
+        } else if (person1Count == person3Count && person1Count > person2Count) {
+            answer = new int[]{1, 3};
+        } else if (person2Count == person3Count && person2Count > person1Count) {
+            answer = new int[]{2, 3};
+        }
+        
+        
+        
+        
+        return answer;
+    }
+}
